6a1d499aa2a83ae7d729136bd6483ba94be8cfd6
Paweł Miłczak <jogobello4@gmail.com>
2020-05-08 16:03:33 +0200

    Connect contact to cloud function, add validation

--- src/components/indexSections/Contact/Contact.js
+++ src/components/indexSections/Contact/Contact.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import axios from "axios"
 
 import { Mail } from "@styled-icons/entypo/Mail"
 import { PhoneAlt } from "@styled-icons/fa-solid/PhoneAlt"
@@ -17,8 +18,86 @@ import {
 } from "components/indexSections/Contact/Contact.styles.js"
 
 const Contact = ({ forwardedRef }) => {
-  // const [email, setEmail] = useState("")
-  // const [message, setMessage] = useState("")
+  const [formValue, setFormValue] = useState({
+    email: "",
+    message: "",
+  })
+  const [submitting, setSubmitting] = useState(false)
+  const [buttonText, setButtonText] = useState("Wyślij")
+  const [errors, setErrors] = useState({
+    email: "",
+    message: "",
+  })
+
+  const handleChange = e => {
+    setFormValue({
+      ...formValue,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const validateEmail = email => {
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(email)
+  }
+
+  const handleBlurEmail = e => {
+    const isCorrect = validateEmail(formValue.email)
+
+    if (formValue.email === "") {
+      setErrors({
+        ...errors,
+        email: "Podaj adres email",
+      })
+    } else if (!isCorrect) {
+      setErrors({
+        ...errors,
+        email: "Niepoprawny adres email",
+      })
+    } else {
+      setErrors({
+        ...errors,
+        email: "",
+      })
+    }
+  }
+
+  const handleBlurMessage = e => {
+    if (formValue.message === "") {
+      setErrors({
+        ...errors,
+        message: "Wpisz treść wiadomości",
+      })
+    } else {
+      setErrors({
+        ...errors,
+        message: "",
+      })
+    }
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+
+    if (!submitting) {
+      console.log(formValue)
+
+      axios
+        .post(
+          "https://us-central1-gatsby-portfolio-a09a3.cloudfunctions.net/sendEmail",
+          formValue
+        )
+        .then(res => {
+          setSubmitting(true)
+          console.log(res)
+          setButtonText("Wysłano")
+        })
+        .catch(err => {
+          console.log(err)
+          setSubmitting(true)
+        })
+    }
+  }
 
   return (
     <StyledWrapper ref={forwardedRef}>
@@ -48,10 +127,29 @@ const Contact = ({ forwardedRef }) => {
           </ContactWrapper>
         </InnerContainer>
 
-        <FormWrapper>
-          <StyledInput type="email" placeholder="Twój email" />
-          <StyledTextArea placeholder="Zostaw wiadomość" />
-          <StyledButton>Wyślij</StyledButton>
+        <FormWrapper onSubmit={handleSubmit}>
+          <StyledInput
+            name="email"
+            placeholder="Twój email"
+            value={formValue.email}
+            onChange={handleChange}
+            onBlur={handleBlurEmail}
+          />
+
+          <StyledTextArea
+            placeholder="Zostaw wiadomość"
+            name="message"
+            value={formValue.message}
+            onChange={handleChange}
+            onBlur={handleBlurMessage}
+          />
+
+          {errors.email && <Paragraph>{errors.email}</Paragraph>}
+          {errors.message && <Paragraph>{errors.message}</Paragraph>}
+
+          <StyledButton disable={submitting} type="submit">
+            {buttonText}
+          </StyledButton>
         </FormWrapper>
       </Container>
     </StyledWrapper>
